0a0bb46b587587722efc110e04ab4d845de26b5c
Josue Montano <josuemontanoa@gmail.com>
2015-04-27 12:52:49 -0400

    Added posts table

--- demonstrare/models.py
+++ demonstrare/models.py
@@ -4,3 +4,13 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(100), nullable=False)
+    body = Column(Text)
+    created = Column(DateTime, nullable=False, default=datetime.utcnow)
+    edited = Column(DateTime, default=datetime.utcnow)
+    is_published = Column(Boolean, nullable=False, default=True)
