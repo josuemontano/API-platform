a45ab2523fad93617911c40a401c262afc4572b1
Josue Montano <josuemontanoa@gmail.com>
2015-04-27 18:07:39 -0400

    Create menu controller

--- demonstrare/static/js/controllers.js
+++ demonstrare/static/js/controllers.js
@@ -1,4 +1,9 @@
 angular.module('demo.controllers', ['demo.services'])
+    .controller('MenuCtrl', ['$scope', '$auth', function($scope, $auth) {
+        $scope.isAuthenticated = function() {
+            return $auth.isAuthenticated();
+        };
+    }])
     .controller('HomeCtrl', ['$scope', function($scope) {
         $scope.message = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
     }])
