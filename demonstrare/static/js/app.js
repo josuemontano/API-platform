6c985381341a8ee9d2686887d7d4faf617f5ec61
Josue Montano <josuemontanoa@gmail.com>
2015-04-27 17:51:22 -0400

    Use satellizer for auth

--- demonstrare/static/js/app.js
+++ demonstrare/static/js/app.js
@@ -1,6 +1,17 @@
-angular.module('demo', ['demo.controllers', 'demo.services', 'demo.directives', 'ui.router', 'ngResource'])
-    .config(['$resourceProvider', '$stateProvider', '$urlRouterProvider', function($resourceProvider, $stateProvider, $urlRouterProvider) {
+angular.module('demo', ['demo.controllers', 'demo.services', 'demo.directives', 'ui.router', 'satellizer', 'ngResource'])
+    .config(['$resourceProvider', '$authProvider', '$stateProvider', '$urlRouterProvider', function($resourceProvider, $authProvider, $stateProvider, $urlRouterProvider) {
         $resourceProvider.defaults.stripTrailingSlashes = false;
+
+        $authProvider.facebook({
+            clientId: '439130886253368',
+            redirectUri: window.location.origin + '/auth/facebook',
+        });
+        $authProvider.google({
+            clientId: '388525728293-bu96shbpa47dpc92efgsemsd6ftu7gj7.apps.googleusercontent.com',
+            redirectUri: window.location.origin + '/auth/google',
+        });
+        $authProvider.loginRedirect = '/dashboard';
+        $authProvider.loginUrl = '/login';
 
         $stateProvider
         .state('home', {
@@ -8,11 +19,18 @@ angular.module('demo', ['demo.controllers', 'demo.services', 'demo.directives', 
             templateUrl: 'static/ui/home.html',
             controller: 'HomeCtrl'
         })
+        // Login & logout
         .state('login', {
             url:'/login',
             templateUrl: 'static/ui/login.html',
             controller: 'LoginCtrl'
         })
+        .state('logout', {
+            url:'/logout',
+            template: null,
+            controller: 'LogoutCtrl'
+        })
+        // Dashboard (private)
         .state('dashboard', {
             url:'/dashboard',
             templateUrl: 'static/ui/dashboard/index.html',
--- demonstrare/static/js/controllers.js
+++ demonstrare/static/js/controllers.js
@@ -2,9 +2,18 @@ angular.module('demo.controllers', ['demo.services'])
     .controller('HomeCtrl', ['$scope', function($scope) {
         $scope.message = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
     }])
-    .controller('LoginCtrl', ['$scope', function($scope) {
-        
+    // Login & logout
+    .controller('LoginCtrl', ['$scope', '$auth', function($scope, $auth) {
+        $scope.authenticate = function(provider) {
+            $auth.authenticate(provider);
+        };
     }])
+    .controller('LogoutCtrl', ['$auth', function($auth) {
+        if (!$auth.isAuthenticated())
+            return;
+        $auth.logout();
+    }])
+    // Dashboard (private)
     .controller('DashboardCtrl', ['$scope', 'Post', function($scope, Post) {
         Post.query(function(data) {
             $scope.posts = data;
