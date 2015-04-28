39ac0119528aa5683be3cebf778975e9df7a62d6
Josue Montano <josuemontanoa@gmail.com>
2015-04-27 20:29:44 -0400

    Create user model

--- demonstrare/models.py
+++ demonstrare/models.py
@@ -5,6 +5,29 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(150), unique=True, nullable=False)
+    password = Column(String(150))
+    display_name = Column(String(150))
+    facebook = Column(String(120))
+    google = Column(String(120))
+    twitter = Column(String(120))
+
+    def __init__(self, email=None, facebook=None, google=None, twitter=None):
+        if email:
+            self.email = email.lower()
+        if facebook:
+            self.facebook = facebook
+        if google:
+            self.google = google
+        if twitter:
+            self.twitter = twitter
+
+
 class Post(Base):
     __tablename__ = 'posts'
 
