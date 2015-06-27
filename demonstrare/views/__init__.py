90eaec2b62b9046e97645d87b4389cec2a9025e1
Josue Montano <josuemontanoa@gmail.com>
2015-06-26 23:37:13 -0400

    Refactor views

--- demonstrare/views/__init__.py
+++ demonstrare/views/__init__.py
@@ -1,1 +1,8 @@
-__author__ = 'josuemontano'
+from datetime import date
+
+from pyramid.view import view_config
+
+
+@view_config(route_name='home', renderer='templates/index.html')
+def index(request):
+    return {'year': date.today().year}
--- demonstrare/views/core.py
+++ demonstrare/views/core.py
@@ -1,33 +1,50 @@
+import json
 import logging
-from datetime import date
 
-from pyramid.view import view_config
+from pyramid.exceptions import HTTPBadRequest
+from pyramid.view import view_defaults, view_config
 from sqlalchemy import desc
 
-from .base import BaseView, register_views
+from .base import BaseView
 from demonstrare.models.core import Post
 from demonstrare.schema.core import PostSchema
 
 log = logging.getLogger(__name__)
 
 
-@view_config(route_name='home', renderer='templates/index.html')
-def index(request):
-    return {'year': date.today().year}
-
-
-@register_views(route='post', collection_route='posts')
+@view_defaults(route_name='post', renderer='json', permission='view')
 class PostsView(BaseView):
     # GET /posts
+    @view_config(request_method='GET', route_name='posts')
     def list(self):
         schema = PostSchema(many=True, only=('id', 'title'))
         ll = self.request.db_session.query(Post).filter_by(is_published=True).order_by(desc(Post.created)).all()
         return schema.dump(ll).data
 
-    # GET /posts/<pk>
+    # GET /posts/<id>
+    @view_config(request_method='GET')
     def detail(self):
         pk = int(self.request.matchdict['id'])
 
         schema = PostSchema()
         item = self.request.db_session.query(Post).get(pk)
         return schema.dump(item).data
+
+    # PUT /profile/<id>
+    @view_config(request_method='PUT')
+    def update(self):
+        pk = int(self.request.matchdict['id'])
+        body = self.request.body.decode("utf-8")
+        body = json.loads(body)
+
+        data, errors = PostSchema(only=('title', 'body')).load(body)
+        if any(errors):
+            return HTTPBadRequest(body=json.dumps(errors))
+
+        log.info('User %d is updating the post "%s"', self.userid, data.title)
+        post = self.request.db_session.query(Post).get(pk)
+        post.title = data.title
+        post.body = data.body
+
+        schema = PostSchema()
+        return schema.dump(post).data
