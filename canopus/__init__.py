0e0873bf6f5e957b097f473160c84e742eccc92b
Josue Montano <josuemontanoa@gmail.com>
2016-10-25 23:03:28 -0400

    Refactor routes config

--- canopus/__init__.py
+++ canopus/__init__.py
@@ -5,24 +5,6 @@ from .auth.policy import JWTAuthenticationPolicy
 from .views.security import groupfinder, RootFactory
 
 
-def config_routes(config):
-    """
-    Add home, auth routes and RESTful routes
-
-    :param config: The pyramid ``Configurator`` object for your app.
-    :type config: ``pyramid.config.Configurator``
-    """
-    config.add_route('home', '/')
-    config.add_route('oauth2-google', '/auth/google')
-    config.add_route('oauth2-facebook', '/auth/facebook')
-    config.add_route('oauth2-live', '/auth/live')
-    # REST Resources
-    config.add_route('posts', '/api/v1/posts')
-    config.add_route('post', '/api/v1/posts/{id}')
-
-    config.scan()
-
-
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
@@ -34,11 +16,11 @@ def main(global_config, **settings):
     config.include('.models')
     config.include('.tasks')
     config.include('.mailer')
+    config.include('.routes')
+
     config.include('pyramid_jinja2')
-
     config.add_renderer('.html', 'pyramid_jinja2.renderer_factory')
-    config.add_static_view('static', 'static', cache_max_age=3600)
 
-    config_routes(config)
+    config.scan()
 
     return config.make_wsgi_app()
---
+++ canopus/routes.py
@@ -0,0 +1,15 @@
+def includeme(config):
+    """Add home, auth and API routes.
+
+    :param config: The pyramid ``Configurator`` object for your app.
+    :type config: ``pyramid.config.Configurator``
+    """
+    config.add_route('home', '/')
+    config.add_route('oauth2-google', '/auth/google')
+    config.add_route('oauth2-facebook', '/auth/facebook')
+    config.add_route('oauth2-live', '/auth/live')
+
+    config.add_route('posts', '/api/v1/posts')
+    config.add_route('post', '/api/v1/posts/{id}')
+
+    config.add_static_view('static', 'static', cache_max_age=3600)
