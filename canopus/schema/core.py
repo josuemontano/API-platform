b5fe71191bc7c39996d526132720a22c3967b1cf
Josue Montano <josuemontanoa@gmail.com>
2016-10-26 00:09:53 -0400

    Fix post schema for latest marshmallow release

--- canopus/schema/core.py
+++ canopus/schema/core.py
@@ -1,13 +1,19 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, post_load
 
-from ..models.core import Post
+from ..models import Post
 
 
 class PostSchema(Schema):
+    __model__ = Post
+
     id = fields.Integer()
     title = fields.String()
     body = fields.String()
     is_published = fields.Boolean()
 
+    class Meta:
+        ordered = True
+
+    @post_load
     def make_object(self, data):
-        return Post(**data)
+        return self.__model__(**data)
