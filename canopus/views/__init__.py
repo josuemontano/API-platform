9d1059bc4cf28b9650bb6386cb5665bfb9b2c138
Josue Montano <josuemontanoa@gmail.com>
2016-10-25 23:35:42 -0400

    Throw 500 error on SQLAlchemy integrity error

--- canopus/views/__init__.py
+++ canopus/views/__init__.py
@@ -1,8 +1,10 @@
 import os
 from datetime import date
 
+from pyramid.httpexceptions import HTTPBadRequest
 from pyramid.response import FileResponse
 from pyramid.view import view_config
+from sqlalchemy.exc import IntegrityError
 
 
 @view_config(route_name='home', renderer='templates/index.html')
@@ -15,3 +17,8 @@ def robots(request):
     here = os.path.abspath(os.path.dirname(__file__))
     response = FileResponse(here + '/../static/robots.txt', request=request)
     return response
+
+
+@view_config(context=IntegrityError)
+def bad_request(exc, request):
+    raise HTTPBadRequest()
