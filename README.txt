7d122ba55286c577e2a603935a89395a6d8c8264
Josue Montano <josuemontanoa@gmail.com>
2016-10-26 00:30:47 -0400

    Remove CHANGES.txt and README.txt

--- README.txt
+++
@@ -1,1 +0,0 @@
-canopus README
--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, 'README.md')) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
 
 requires = [
     'pyramid',
@@ -30,17 +28,17 @@ requires = [
 setup(name='canopus',
       version='1.1',
       description='canopus',
-      long_description=README + '\n\n' + CHANGES,
+      long_description=README,
       classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pyramid",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
       ],
       author='Josue Montano',
       author_email='josuemontanoa@gmail.com',
       url='',
-      keywords='web pyramid pylons rest api angularjs',
+      keywords='web pyramid pylons rest api angularjs'
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
